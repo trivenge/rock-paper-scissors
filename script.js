9e04140fe3a9cb194b0f69d92f5e0cd87b0349be
Michael <10603177+trivenge@users.noreply.github.com>
2023-03-15 08:19:37 -0400

    clean & organize

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
+
 function getComputerChoice() {
-  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+  let randomChoice = choices[Math.floor(Math.random() * choices.length)];
   return randomChoice;
 }
+const computerSelection = getComputerChoice();
 
-console.log(getComputerChoice());
+let playerSelection = 
+
+let draw = {
+  switch (if playerSelection && computerSelection == 'Rock' || 'Paper' || 'Scissors') {
+  case 'Rock':
+    return "Rock does not beat Rock, draw!";
+    break;
+  case 'Scissors':
+    return "Scissors does not beat Scissors, draw!";
+    break;
+  case 'Paper':
+    return "Paper does not beat Paper, draw!";
+    break;
+  }
+};  
+
+function winner(playerSelection, computerSelection) {
+  if playerSelection == true && computerSelection == false {
+    return "Player "
+    else 
+  }
+};
+
+
+
+
+function (playerSelection, computerSelection) {
+
+ 
+  if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+    return "Rock beats Scissors,  wins!"
+  }
+  if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+    return "Rock beats Scissors,  wins!"
+  }
+  if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+    return "Rock beats Scissors,  wins!"
+  }
+ };
+
+
+};
