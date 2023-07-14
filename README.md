baafb445e18eb922b77c983456ce1b960d569aad
Michael <10603177+trivenge@users.noreply.github.com>
2023-07-14 09:09:37 -0400

    Player Choice with Buttons added

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # rock-paper-scissors
+
+What I learned from the project?
+Initially:
+
+After learning DOM and DOM manipulation with javascript:
+
+
+
+Credits to these sources for images:
+
+Computer Hands: https://www.pngmart.com/image/131878
+
+Rock: https://pixabay.com/photos/png-clipping-rock-graphics-pierre-1345905/
+
+Scissors: https://purepng.com/photo/20931/objects-scissors
+
+Computer Scissors: https://purepng.com/photo/20918/objects-scissors
+
+Computer Rock: https://purepng.com/photo/13768/nature-stones-and-rocks
+
+Paper: https://freepngimg.com/png/3330-paper-sheet-png-image
+
+Computer Paper: https://freepngimg.com/png/3318-paper-sheet-png-image
+Player Hands: https://www.pngfind.com/mpng/miwiT_hands-png-download-image-transparent-hands-png-png/
+
+background: https://www.freepik.com/free-vector/hand-painted-watercolor-abstract-watercolor-background_15441995.htm#page=3&query=background&position=1&from_view=keyword&track=sph
--- script.js
+++ script.js
@@ -1,95 +1,72 @@
 // array of the game choices
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = ['rock', 'paper', 'scissors']
 
 // Randomly selects (Math.random) from the array of choices (length) from the list of choices and
 // generates a random choice for the computer selection
 function getComputerChoice() {
-  let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-  return randomChoice;
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+  return randomChoice
 }
 
-// prompt user input from player and check if input is null or a string and force user to input again
-function playerSelection() {
-  let playerInput = prompt('Enter your choice of:\nRock, Paper, or Scissors?');
-
-  if (playerInput === '') {
-    alert(
-      "Please type 'Rock, Paper, or Scissors' or press cancel if you do not wish to play"
-    );
-    return playerSelection();
-  }
-  // Capitalize first letter of playerInput and join with lower case slice
-  let finalPlayerInput =
-    playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
-  // If finalPlayerInput is the same as one of the game choices with or without case sensitivity
-  // return the final player input fixed to the first letter capitalized + lower case of remaining letters
-  if (choices.includes(finalPlayerInput)) {
-    return finalPlayerInput;
+//Rules of the game
+function game(playerChoice, computerSelection) {
+  if (
+    (playerChoice === 'rock' && computerSelection === 'scissors') ||
+    (playerChoice === 'scissors' && computerSelection === 'paper') ||
+    (playerChoice === 'paper' && computerSelection === 'rock')
+  ) {
+    return (document.querySelector('div.result').textcontent = 'Player Wins!')
+  } else if (playerChoice === computerSelection) {
+    return (document.querySelector('div.result').textContent = "It's a DRAW!")
   } else {
-    // Otherwise, prompt the user to enter a valid option
-    alert("Make sure to correctly enter either 'Rock, Paper, or Scissors!'");
-    return playerSelection();
+    return (document.querySelector('div.result').textContent = 'Computer Wins!')
   }
 }
+//Gather player choice input
+function playerFinalInput(playerChoice) {
+  const computerSelection = getComputerChoice()
+  game(playerChoice, computerSelection)
 
-function winner(playerChoice, computerSelection) {
-  if (playerChoice == 'Rock' && computerSelection == 'Scissors') {
-    return 'Rock beats Scissors, Player wins!';
-  }
-  if (playerChoice == 'Scissors' && computerSelection == 'Paper') {
-    return 'Scissors beats Paper, Player wins!';
-  }
-  if (playerChoice == 'Paper' && computerSelection == 'Rock') {
-    return 'Paper beats Rock, Player wins!';
-  }
-  if (computerSelection == 'Rock' && playerChoice == 'Scissors') {
-    return 'Rock beats Scissors, Computer wins!';
-  }
-  if (computerSelection == 'Scissors' && playerChoice == 'Paper') {
-    return 'Scissors beats Paper, Computer wins!';
-  }
-  if (computerSelection == 'Paper' && playerChoice == 'Rock') {
-    return 'Paper beats Rock, Computer wins!';
-  }
+  console.log(playerChoice)
+  console.log(computerSelection)
 }
 
-//function draw determines if playerchoice is equal to computer, if so, player does not beat computer, so draw!
-function draw(playerChoice, computerSelection) {
-  if (playerChoice == computerSelection) {
-    return `Player's choice of ${playerChoice} does not beat the Computer's choice of ${computerSelection}: Draw!`;
-  }
+////Play 5 games
+// try {
+//   for (let i = 0; i < 5; i++) {
+//     const result = game();
+//   }
+// } catch (error) {
+//   console.log(error);
+// }
+
+//DOM for the rock, paper, and scissors game
+const rockImage = document.querySelector('.rock')
+const paperImage = document.querySelector('.paper')
+const scissorsImage = document.querySelector('.scissors')
+
+const rock = rockImage
+const paper = paperImage
+const scissors = scissorsImage
+
+// const buttons = document.querySelectorAll('button')
+
+//addEventListener and console log of click/DOM target list (classList)
+
+function imageClick(event) {
+  const eventClick = event.target
+  console.log(eventClick.classList)
 }
 
-function game() {
-  const computerSelection = getComputerChoice();
-  const playerChoice = playerSelection();
-  const resultOfDraw = draw(playerChoice, computerSelection);
-  const resultOfWinner = winner(playerChoice, computerSelection);
+rockImage.addEventListener('click', () => playerFinalInput('rock'))
+paperImage.addEventListener('click', () => playerFinalInput('paper'))
+scissorsImage.addEventListener('click', () => playerFinalInput('scissors'))
 
-  if (playerChoice === null) {
-    console.log('Player input is null. Game terminated.');
-    alert('Game terminated. Please restart the game to play again.');
-    throw new Error('Game terminated');
-  }
+// if (imageClick) {
+//   imageClick == 'rock' || rock
+//   return 'rock wins!'
+// }
 
-  console.log(playerChoice);
-  console.log(computerSelection);
-
-  if (resultOfWinner) {
-    console.log(resultOfWinner);
-    alert(resultOfWinner);
-    return resultOfWinner;
-  } else if (resultOfDraw) {
-    console.log(resultOfDraw);
-    alert(resultOfDraw);
-    return resultOfDraw;
-  }
-}
-
-try {
-  for (let i = 0; i < 5; i++) {
-    const result = game();
-  }
-} catch (error) {
-  console.log(error);
-}
+//Results are entered into div for display
+const result = (document.querySelector('div.result').textContent =
+  "The `${playerFinalInput}` Wins!'")
